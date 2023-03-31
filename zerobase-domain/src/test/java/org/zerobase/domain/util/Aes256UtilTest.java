3815fe1451b4889b9d99aba5bff90f16958415c0
trsoo <trsoo24@naver.com>
2023-04-01 00:57:32 +0900

    feat : [004-LOGIN_CUSTOMER] 커스터머에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/* 에 대한 token filter 적용

---
+++ zerobase-domain/src/test/java/org/zerobase/domain/util/Aes256UtilTest.java
@@ -0,0 +1,20 @@
+package org.zerobase.domain.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class Aes256UtilTest {
+
+    @Test
+    void encrypt() {
+        String encrypt = Aes256Util.encrypt("Hello world");
+        assertEquals(Aes256Util.decrypt(encrypt), "Hello world");
+    }
+
+//
+//    @Test
+//    void decrypt() {
+//    }
+}
