489588fe69e70ab9d838150cd0b1b4f4203d4cd2
trsoo <trsoo24@naver.com>
2023-04-02 17:53:16 +0900

    feat : [013-ADD-CART] 카트에 대한 추가 구현

---
+++ order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
@@ -0,0 +1,27 @@
+package com.zerobase.cms.order.controller;
+
+import com.zerobase.cms.order.application.CartApplication;
+import com.zerobase.cms.order.domain.product.AddProductCartForm;
+import com.zerobase.cms.order.domain.redis.Cart;
+import com.zerobase.cms.order.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.zerobase.domain.config.JwtAuthenticationProvider;
+
+@RestController
+@RequestMapping("/customer/cart")
+@RequiredArgsConstructor
+public class CustomerCartController {
+
+    // 임시 코드
+    private final CartService cartService;
+    private final JwtAuthenticationProvider provider;
+
+    @PostMapping
+    public ResponseEntity<Cart> addCart(
+            @RequestHeader(name = "X-AUTH-TOKEN") String token,
+            @RequestBody AddProductCartForm form) {
+        return ResponseEntity.ok(cartService.addCart(provider.getUserVo(token).getId(), form));
+    }
+}
